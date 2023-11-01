bb792fc23f55338bc509f3d40f068ba2c11fec26
Yunguer Dvorak <yunguerdvorak@hotmail.com>
2023-11-01 12:18:36 -0300

    Add part of the NPC Dialogue System with Quest and XML Reader

--- TCC/Assets/Scripts/NPC/NPC.cs
+++ TCC/Assets/Scripts/NPC/NPC.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 namespace ProjetoTCC
 {
     public class NPC : MonoBehaviour
     {
+        [SerializeField]
+        private string xmlName;
+
         public static Action OnAnimationEnded;
 
         [SerializeField]
@@ -17,13 +22,33 @@ namespace ProjetoTCC
         private TMP_Text textBox;
 
         [SerializeField]
-        private int dialogueID;
+        private GameObject answerPainel;
+
+        private bool answeringQuestion;
 
         [SerializeField]
-        private string[] dialogue_1;
+        private Button btn_A;
+
+        [SerializeField]
+        private TMP_Text textButtonA, textButtonB;
+
+        [SerializeField]
+        private int dialogueLineID;
+
+        [SerializeField]
+        private List<string> dialogue_1;
         
         [SerializeField]
-        private string[] dialogue_2;
+        private List<string> dialogue_2;
+
+        [SerializeField]
+        private List<string> dialogue_3;
+
+        [SerializeField]
+        private List<string> dialogue_4;
+
+        [SerializeField]
+        private List<string> answerQuestion;
 
         [SerializeField]
         private List<string> chatLines;
@@ -36,9 +61,10 @@ namespace ProjetoTCC
         void Start()
         {
             npcCanvas.SetActive(false);
+            answerPainel.SetActive(false);
             isTalking = false;
 
-           
+            LoadDialogueData();
         }
 
 
@@ -51,7 +77,7 @@ namespace ProjetoTCC
         {
             if(isTalking == false)
             {
-                dialogueID = 0;
+                dialogueLineID = 0;
 
                 if(chatLines != null)
                 {
@@ -65,9 +91,9 @@ namespace ProjetoTCC
                 isTalking = true;
                 OnAnimationEnd();
             }
-            else
+            else if(isTalking == true && answeringQuestion == false)
             {
-                dialogueID += 1;
+                dialogueLineID += 1;
                 Dialogue();
                 OnAnimationEnd();
             }
@@ -75,19 +101,34 @@ namespace ProjetoTCC
 
         public void Dialogue()
         {
-            if(dialogueID < chatLines.Count)
+            if(dialogueLineID < chatLines.Count)
             {
-                textBox.text = chatLines[dialogueID];
+                textBox.text = chatLines[dialogueLineID];
+
+                if(chatID == 0 && dialogueLineID == 2)
+                {
+                    textButtonA.text = answerQuestion[0];
+                    textButtonB.text = answerQuestion[1];
+
+                    answeringQuestion = true;
+                    answerPainel.SetActive(true);
+                    btn_A.Select();
+                }
             }
             else
             {
                 switch(chatID)
                 {
                     case 0:
-                        chatID = 1;
+
                         break;
 
                     case 1:
+                        chatID = 3;
+                        break;
+
+                    case 2:
+                        chatID = 0;
                         break;
                 }
 
@@ -99,6 +140,7 @@ namespace ProjetoTCC
 
         public void PreparDialogue()
         {
+            chatLines.Clear();
             switch (chatID)
             {
                 case 0:
@@ -118,6 +160,88 @@ namespace ProjetoTCC
                     }
 
                     break;
+
+                case 2:
+
+                    foreach (string s in dialogue_3)
+                    {
+                        chatLines.Add(s);
+                    }
+
+                    break;
+
+                case 3:
+
+                    foreach (string s in dialogue_4)
+                    {
+                        chatLines.Add(s);
+                    }
+
+                    break;
+            }
+        }
+
+        public void AnswerAButton()
+        {
+            chatID = 1;
+            PreparDialogue();
+            dialogueLineID = 0;
+
+            answeringQuestion = false;
+            answerPainel.SetActive(false);
+
+            Dialogue();
+            OnAnimationEnd();
+        }
+
+        public void AnswerBButton()
+        {
+            chatID = 2;
+            PreparDialogue();
+            dialogueLineID = 0;
+
+            answeringQuestion = false;
+            answerPainel.SetActive(false);
+
+            Dialogue();
+            OnAnimationEnd();
+        }
+
+        public void LoadDialogueData()
+        {
+            TextAsset xmlData = (TextAsset)Resources.Load(xmlName);
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.LoadXml(xmlData.text);
+
+            foreach(XmlNode dialogue in xmlDocument["chats"].ChildNodes)
+            {
+                string dialogueName = dialogue.Attributes["name"].Value;
+
+                foreach(XmlNode f in dialogue["lines"].ChildNodes)
+                {
+                    switch(dialogueName)
+                    {
+                        case "dialogue_1":
+                            dialogue_1.Add(f.InnerText);
+                            break;
+
+                        case "dialogue_2":
+                            dialogue_2.Add(f.InnerText);
+                            break;
+
+                        case "dialogue_3":
+                            dialogue_3.Add(f.InnerText);
+                            break;
+
+                        case "dialogue_4":
+                            dialogue_4.Add(f.InnerText);
+                            break;
+
+                        case "resposta_1":
+                            answerQuestion.Add(f.InnerText);
+                            break;
+                    }
+                }
             }
         }
 
